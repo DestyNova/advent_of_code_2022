f294ba4c8ee36245b543b13b52340f1e659dbb98
Oisín Mac Fhearaí <denpashogai@gmail.com>
2022-12-01 13:11:58 +0000

    Day 1: remove ugly hack for superfluous empty strings using strip + clean up syntax, thanks to Michal Maršálek for the tips!

--- 1/part2.nim
+++ 1/part2.nim
@@ -2,11 +2,12 @@ import algorithm,math,sequtils,strutils
 
 echo stdin
       .readAll
+      .strip
       .split("\n\n")
       .mapIt(
         it
-          .split()
-          .mapIt(("0" & it).parseInt)
+          .split
+          .map(parseInt)
           .sum)
       .sorted(Descending)[0..2]
       .sum
