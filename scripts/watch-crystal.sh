5844f02472cac012724948fc5ec61731067a8c44
Oisín Mac Fhearaí <denpashogai@gmail.com>
2022-12-02 07:30:57 +0000

    Crystal watch script: don't build in release mode
    ...because it takes like 12 seconds instead of 2 seconds. If I need "release" speed, I can use the standalone build script, but that probably won't be necessary (unless I'm bruteforcing)

--- scripts/watch-crystal.sh
+++ scripts/watch-crystal.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-$SCRIPT_DIR/watch.sh '*.cr' bash -c "\"time crystal run --release part$1.cr < $2\""
+$SCRIPT_DIR/watch.sh '*.cr' bash -c "\"time crystal run part$1.cr < $2\""
