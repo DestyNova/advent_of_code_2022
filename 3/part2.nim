ffa01a289888aa21228257a166aef5004f1cbbbd
Oisín Mac Fhearaí <denpashogai@gmail.com>
2022-12-03 05:58:13 +0000

    Day 3: Rucksack Reorganization (Crystal)

---
+++ 3/part2.nim
@@ -0,0 +1,9 @@
+# var score = 0
+# 
+# for line in stdin.lines:
+#   if line != "":
+#     let outcome = getOutcome(line[0], line[2])
+#     let movePoints = getMovePoints(line[2])
+#     score += outcome + movePoints
+# 
+# echo score
