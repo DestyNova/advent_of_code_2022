49c6731c161d8e92ac9001baa409b7e99ebdc56e
Oisín Mac Fhearaí <denpashogai@gmail.com>
2022-12-17 08:20:54 +0000

    Day 17: pretty sweet

--- 16/part2_bfs_intpack.nim
+++ 16/part2_bfs_intpack.nim
@@ -1,19 +1,20 @@
 import std/[algorithm, sequtils, strformat, strscans, strutils, sets, heapqueue, tables, sugar, math]
 
-# vertex needs to encode my valve label, elephant's valve, pressure released, mins remaining and list of unturned??
-type Vertex = (int,int,int,int,int)
+# vertex needs to encode my valve label, elephant's valve, pressure released, mins remaining and list of unturned
+type Vertex = (int8,int8,int,int,int)
 const Inf = high(int)
 
-var valveNames: Table[string, (int, seq[string], int)]
-var valves: Table[int, (int, seq[int])]
+var valveNames: Table[string, (int, seq[string], int8)]
+var valves: Table[int8, (int, seq[int8])]
+var useless: set[int8]
 
-proc encodeValves(keys: seq[int]): int =
+proc encodeValves(keys: seq[int8]): int =
   var x = 0
   for i in keys: x = x or (1 shl i)
   x
 
-proc holds(x, key: int): bool = (x and (1 shl key)) != 0
-proc without(x: int, keys: seq[int]): int =
+proc holds(x: int, key: int8): bool = (x and (1 shl key)) != 0
+proc without(x: int, keys: seq[int8]): int =
   var y = x
   for k in keys:
     y = y xor (1 shl k)
@@ -21,7 +22,6 @@ proc without(x: int, keys: seq[int]): int =
 
 proc getNeighbours(v: Vertex): seq[Vertex] =
   let
-    # (myV, elephantV, released, mins, unturned, moves) = v
     (myV, elephantV, released, mins, unturned) = v
     (flowRate1, ns1) = valves[myV]
     (flowRate2, ns2) = valves[elephantV]
@@ -33,7 +33,6 @@ proc getNeighbours(v: Vertex): seq[Vertex] =
 
   # we both turn a valve, but it must be two different valves
   if unturned.holds(myV) and unturned.holds(elephantV) and myV != elephantV and (valves[myV][0] > 0 or valves[elephantV][0] > 0):
-    # vs.add((myV, elephantV, m*flowRate1 + m*flowRate2 + released, m, unturned.without([myV, elephantV].toHashSet),moves & @[fmt"T {myV}, T {elephantV}"]))
     vs.add((myV, elephantV, m*flowRate1 + m*flowRate2 + released, m, unturned.without(@[myV, elephantV])))
 
   # otherwise, at least one of us moves
@@ -50,7 +49,6 @@ proc getNeighbours(v: Vertex): seq[Vertex] =
   # I turn this valve and elephant moves to valve n2
   for n2 in ns2:
     if unturned.holds(myV) and valves[myV][0] > 0:
-      # vs.add((myV, n2, m*flowRate1 + released, m, unturned.without([myV].toHashSet),moves & @[fmt"T {myV}, > {n2}"]))
       vs.add((myV, n2, m*flowRate1 + released, m, unturned.without(@[myV])))
 
   return vs
@@ -58,8 +56,8 @@ proc getNeighbours(v: Vertex): seq[Vertex] =
 proc getApproximateOptimum(unturned: int, mins: int): int =
   let rates = collect(
     for k in 0..63:
-      if unturned.holds(k):
-        valves[k][0]
+      if unturned.holds(int8(k)):
+        valves[int8(k)][0]
     ).sortedByIt(-it)
   # let rates = unturned.mapIt(valves[it][0]).sortedByIt(-it)
   # echo fmt"rates: {rates}"
@@ -78,9 +76,16 @@ proc getApproximateOptimum(unturned: int, mins: int): int =
 proc `<`(a, b: Vertex): bool =
   # a[2] >= b[2]
   let
-    aPossible = 3*a[2] + 2*getApproximateOptimum(a[4], a[3])
-    bPossible = 3*b[2] + 2*getApproximateOptimum(b[4], b[3])
+    aPossible = 2*a[2] + 1*getApproximateOptimum(a[4], a[3])
+    bPossible = 2*b[2] + 1*getApproximateOptimum(b[4], b[3])
   aPossible > bPossible
+
+proc notUseless(unturned: int): bool =
+  var closedValves: set[int8]
+  for k in 0..63:
+    if unturned.holds(int8(k)):
+      closedValves.incl(int8(k))
+  return closedValves != useless
 
 # highest result so far... 2082... 2297, 2543, 2615
 proc bfs(start: Vertex): (int, seq[string]) =
@@ -107,7 +112,7 @@ proc bfs(start: Vertex): (int, seq[string]) =
       # bestMoves = moves
       echo fmt"new best v: {v}, maxReleased: {maxReleased}, bestPossible: {bestPossible}"
 
-    if bestPossible > maxReleased:
+    if bestPossible > maxReleased and notUseless(unturned):
       for u in getNeighbours(v):
         let
           # (_, _, uReleased, _, _, uMoves) = u
@@ -121,7 +126,7 @@ proc bfs(start: Vertex): (int, seq[string]) =
 
   (maxReleased, bestMoves)
 
-var i = 0
+var i: int8 = 0
 for line in stdin.lines:
   var
     valve: string
@@ -134,6 +139,8 @@ for line in stdin.lines:
   echo fmt"valve: {valve}, flowRate: {flowRate}, destinations: {destinations}"
 
   valveNames[valve] = (flowRate, destinations.split(", "), i)
+  if flowRate == 0:
+    useless.incl(i)
   i += 1
 
 for k,v in valveNames:
@@ -143,12 +150,12 @@ for k,v in valveNames:
 echo valves
 
 let
-  moves: seq[string] = @[]
   # start: Vertex = ("AA","AA",0,26,valves.keys.toSeq.toHashSet,moves)
   startValve = valveNames["AA"][2]
   start: Vertex = (startValve,startValve,0,26,encodeValves(valves.keys.toSeq))
 echo fmt"neighbours of start node: {getNeighbours(start)}"
 echo fmt"initial approximate optimum: {getApproximateOptimum(encodeValves(valves.keys.toSeq), 30)}"
 echo fmt"starting unturned: {valves.keys.toSeq}"
+echo fmt"useless valves: {useless}"
 echo bfs(start)
 echo fmt"Initial state: {valves.keys.toSeq} => {encodeValves(valves.keys.toSeq):#b}"
